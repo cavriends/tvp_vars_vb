56d70a82ff406066eb9054e63dfd6951947b5323
C.A. Vriends <cavriends@gmail.com>
2020-07-06 22:35:34 +0200

    Added predictions for comparison with TVP-VAR (2013)

--- utils/tvp_models.py
+++ utils/tvp_models.py
@@ -5,14 +5,21 @@ from scipy.stats import norm
 
 class TVPVARModel:
 
-    def __init__(self, X, y):
+    def __init__(self, X, y, p, train_index):
         self.X = X
         self.y = y
         self.T = X.shape[0]
         self.M = X.shape[1]
         self.k = X.shape[2]
+        self.p = p
+        self.train_index = train_index
+        self.T_train = train_index - self.p
         self.initialized_priors = False
         self.initialized_volatility = False
+
+    def create_train(self):
+        self.X_train = self.X[:self.T_train, :]
+        self.y_train = self.y[:self.T_train, :]
 
     def initialize_priors(self, prior='svss', prior_parameters={'tau_0':0.1, 'tau_1':10, 'pi0':0.5},
                           prior_default=True):
@@ -32,14 +39,14 @@ class TVPVARModel:
 
             self.a0_horseshoe = prior_parameters['a0']
             self.b0_horseshoe = prior_parameters['b0']
-            self.lambda_t_horseshoe = np.ones(self.T)
-            self.phi_t = np.ones((self.T, self.k))
+            self.lambda_t_horseshoe = np.ones(self.T_train)
+            self.phi_t = np.ones((self.T_train, self.k))
 
         elif prior == 'lasso':
             if prior_default:
                 prior_parameters = {'lambda_param': 50}
 
-            self.tau_lasso = np.ones((self.T, self.k))
+            self.tau_lasso = np.ones((self.T_train, self.k))
             self.lambda_param = prior_parameters['lambda_param']
 
         self.initialized_priors = True
@@ -56,15 +63,17 @@ class TVPVARModel:
             self.b0_h = 1
         else:
             # sigma_t ~ Gamma(at,bt)
-            self.at = np.ones((self.T, self.M))
+            self.at = np.ones((self.T_train, self.M))
             self.a0 = 1e-2
-            self.bt = np.ones((self.T, self.M))
+            self.bt = np.ones((self.T_train, self.M))
             self.b0 = 1e-2
 
         self.initialized_volatility = True
 
 
     def train(self, iterations=1000, threshold=1.0e-4, print_status=True):
+
+        self.create_train()
 
         if not self.initialized_priors:
             self.initialize_priors()
@@ -77,24 +86,24 @@ class TVPVARModel:
         S0 = 4*np.eye(self.k)
 
         # q_t ~ Gamma(ct,dt)
-        ct = np.ones((self.T,self.k))
-        dt = np.ones((self.T,self.k))
+        ct = np.ones((self.T_train, self.k))
+        dt = np.ones((self.T_train, self.k))
         d0 = 1
         c0 = 25
 
-        self.tv_probs = np.ones((self.T, self.k))
-        self.sigma_t = 0.1 * np.ones((self.T, self.M))
+        self.tv_probs = np.ones((self.T_train, self.k))
+        self.sigma_t = 0.1 * np.ones((self.T_train, self.M))
 
-        mtt  = np.zeros((self.k,self.T))
-        self.mt1t = np.zeros((self.k,self.T))
-        mtt1 = np.zeros((self.k,self.T))
-        Stt1 = np.zeros((self.k,self.k,self.T))
-        Stt  = np.zeros((self.k,self.k,self.T))
+        mtt  = np.zeros((self.k,self.T_train))
+        self.mt1t = np.zeros((self.k,self.T_train))
+        mtt1 = np.zeros((self.k,self.T_train))
+        Stt1 = np.zeros((self.k,self.k,self.T_train))
+        Stt  = np.zeros((self.k,self.k,self.T_train))
 
-        lambda_t = np.zeros((self.T,self.k))
-        q_t      = np.ones((self.T,self.k))
-        Qtilde   = np.zeros((self.k,self.k,self.T))
-        Ftilde   = np.zeros((self.k,self.k,self.T))
+        lambda_t = np.zeros((self.T_train, self.k))
+        q_t      = np.ones((self.T_train, self.k))
+        Qtilde   = np.zeros((self.k,self.k,self.T_train))
+        Ftilde   = np.zeros((self.k,self.k,self.T_train))
     
         offset    = 0.0015
         delta     = 0.9
@@ -102,7 +111,7 @@ class TVPVARModel:
         elapsed_time = 0
         start_time = 0
         counter = 0
-        mt1t_previous = np.ones((self.k,self.T))
+        mt1t_previous = np.ones((self.k,self.T_train))
         difference_parameters = np.zeros(iterations)
 
         while (counter < iterations) & (np.linalg.norm(self.mt1t - mt1t_previous) > threshold):
@@ -124,12 +133,12 @@ class TVPVARModel:
                     clear_output(wait=True)
                     print("Done!")
 
-            self.mt1t = np.zeros((self.k,self.T))
-            self.St1t = np.zeros((self.k, self.k, self.T))
+            self.mt1t = np.zeros((self.k,self.T_train))
+            self.St1t = np.zeros((self.k, self.k, self.T_train))
 
             # Kalman filter
             # ==================| Update \beta_{t} using Kalman filter/smoother
-            for t in range(self.T):
+            for t in range(self.T_train):
                 if self.prior == 'none':
                     Qtilde[:,:,t]  = np.diag(1/(q_t[t,:]))
                     Ftilde[:,:,t]  = np.eye(self.k)
@@ -150,21 +159,21 @@ class TVPVARModel:
                     mtt1[:,t]   = Ftilde[:,:,t]@mtt[:,t-1]
                     Stt1[:,:,t] = Ftilde[:,:,t]@Stt[:,:,t-1]@Ftilde[:,:,t].T + Qtilde[:,:,t]
 
-                Sx              = Stt1[:,:,t]@self.X[t,:].T
-                Kt              = Sx@np.linalg.inv((self.X[t,:] @ Sx + self.sigma_t[t, :]))
-                mtt[:,t]        = mtt1[:,t] + Kt@(self.y[t,:] - self.X[t,:]@mtt1[:,t])
-                Stt[:,:,t]      = (np.eye(self.k) - Kt@self.X[t,:])@Stt1[:,:,t]
+                Sx              = Stt1[:,:,t]@ self.X_train[t, :].T
+                Kt              = Sx@np.linalg.inv((self.X_train[t, :] @ Sx + self.sigma_t[t, :]))
+                mtt[:,t]        = mtt1[:,t] + Kt@(self.y_train[t, :] - self.X_train[t, :] @ mtt1[:, t])
+                Stt[:,:,t]      = (np.eye(self.k) - Kt @ self.X_train[t, :]) @ Stt1[:, :, t]
 
                 # Fixed interval smoother
                 self.mt1t[:,t] = mtt[:,t]
                 self.St1t[:, :, t] = Stt[:, :, t]
 
-            for t in reversed(range(self.T-1)):
+            for t in reversed(range(self.T_train - 1)):
                 C = (Stt[:,:,t]@Ftilde[:,:,t+1])@np.linalg.inv(Stt1[:,:,t+1])
                 self.mt1t[:,t]   = mtt[:,t] + C@(self.mt1t[:,t+1] - mtt1[:,t+1])
                 self.St1t[:, :, t] = Stt[:, :, t] + C @ (self.St1t[:, :, t + 1] - Stt1[:, :, t + 1]) @ C.T
 
-            for t in range(self.T):
+            for t in range(self.T_train):
                 eyeF = (np.eye(self.k) - 2*Ftilde[:,:,t]).T
                 if t == 0:
                     D = self.St1t[:, :, t] + self.mt1t[:, t] @ self.mt1t[:, t].T + (S0 + m0 * m0.T) @ eyeF
@@ -177,7 +186,7 @@ class TVPVARModel:
                 dt[t,:]     = d0 + np.maximum(1e-10,np.diag(D)/2)
                 q_t[t,:]    = ct[t,:]/dt[t,:]
 
-            for t in range(self.T):
+            for t in range(self.T_train):
                 if self.prior == 'svss':
                     l_0           = norm.logpdf(self.mt1t[:,t],np.zeros(self.k),self.tau_0*np.ones(self.k))
                     l_1           = norm.logpdf(self.mt1t[:,t],np.zeros(self.k),self.tau_1*np.ones(self.k))
@@ -198,21 +207,21 @@ class TVPVARModel:
             if self.homoskedastic:
                 for m in range(self.M):
 
-                    self.at_h[m] = self.a0_h + self.T
+                    self.at_h[m] = self.a0_h + self.T_train
 
-                    for t in range(self.T):
-                        updated_b = np.sum(np.power(self.y[t,m]-self.X[t,m].T@self.mt1t[:,t],2))
+                    for t in range(self.T_train):
+                        updated_b = np.sum(np.power(self.y_train[t, m] - self.X_train[t, m].T @ self.mt1t[:, t], 2))
 
                     self.bt_h[m] = self.b0_h + updated_b/2
 
                     self.sigma_t[:, m] = self.bt_h[m] / self.at_h[m]
 
             else:
-                s_tinv = np.zeros((self.T,self.M))
-                for t in range(self.T):
-                    temp = self.X[t,:] @ (self.mt1t[:,t] @self.mt1t[:,t].T + self.St1t[:, :, t]) @ self.X[t, :].T - \
-                           2 * self.X[t, :] @ self.mt1t[:, t] @ self.y[t, :] + \
-                           (1 + offset) * self.y[t, :] @ self.y[t, :].T
+                s_tinv = np.zeros((self.T_train, self.M))
+                for t in range(self.T_train):
+                    temp = self.X_train[t, :] @ (self.mt1t[:, t] @ self.mt1t[:, t].T + self.St1t[:, :, t]) @ self.X_train[t, :].T - \
+                           2 * self.X_train[t, :] @ self.mt1t[:, t] @ self.y_train[t, :] + \
+                           (1 + offset) * self.y_train[t, :] @ self.y_train[t, :].T
 
                     if t == 0:
                         self.at[t,:] = self.a0 + 0.5
@@ -224,9 +233,9 @@ class TVPVARModel:
                     s_tinv[t,:] = np.divide(self.at[t,:],self.bt[t,:])
 
                 # Smooth volatilities
-                phi = np.zeros((self.T,self.M))
-                phi[self.T-1,:] = np.divide(self.at[self.T-1,:],self.bt[self.T-1,:])
-                for t in reversed(range(self.T-1)):
+                phi = np.zeros((self.T_train, self.M))
+                phi[self.T_train - 1, :] = np.divide(self.at[self.T_train - 1, :], self.bt[self.T_train - 1, :])
+                for t in reversed(range(self.T_train - 1)):
                     phi[t,:] = [1-delta]*s_tinv[t,:] + delta*phi[t+1,:]
                 self.sigma_t = 1 / phi
 
@@ -238,4 +247,48 @@ class TVPVARModel:
             # Increase counter
             counter += 1
 
+        self.initialized_priors = False
+        self.initialized_volatility = False
+
         return self.mt1t, self.St1t
+
+    def calculate_predictions(self, total_h):
+
+        self.initial_T_train = self.T_train
+        self.initial_train_index = self.train_index
+
+        self.number_of_predictions = self.T - self.T_train
+        self.prev_pred = np.zeros((self.number_of_predictions, self.M, total_h))
+
+        for t in range(self.number_of_predictions):
+            self.train_index = self.initial_train_index + t
+            self.T_train = self.train_index - self.p
+
+            prev_X = self.X[self.T_train]
+
+            __, __ = self.train(print_status=False)
+
+            for h in range(total_h):
+
+                self.prev_pred[t,:,h] = prev_X@self.mt1t[:,-1]
+                vec_X = prev_X[0, :(self.M * self.p + 1)]
+
+                empty_X = np.zeros((self.M * self.p + 1))
+                empty_X[0] = 1
+
+                for m in range(self.M):
+                    empty_X[(m * self.p + 2):((m + 1) * self.p + 1)] = vec_X[(m * self.p + 1):((m + 1) * self.p)]
+
+                vec_X = empty_X
+                vec_X[1::self.p] = self.prev_pred[t,:,h]
+
+                prev_X = np.zeros((self.M, self.k))
+                for m in range(self.M):
+                    total_lags = self.M * self.p + 1
+                    prev_X[m, m * (total_lags):(m + 1) * total_lags] = vec_X
+
+            self.T_train =  self.initial_T_train
+            self.train_index = self.initial_train_index
+            
+        return self.prev_pred
+
